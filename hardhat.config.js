7655c097b079ad1b737dc656daec48b748d02f8c
VaithiSniper <vaithi.genghiskhan@gmail.com>
2023-03-18 00:41:43 +0530

    hardhat initialised

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: "https://endpoints.omniatech.io/v1/matic/mumbai/public",
+      accounts: [process.env.PRIVATEKEY1]
+    }
+  },
 };
