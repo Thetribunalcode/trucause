ce7015e6f118c0a2adef7200dd134abde30a47fd
parth <parthsheth469@gmail.com>
2023-03-26 19:20:46 +0530

    merge

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('dotenv').config({ path: __dirname + '/.env.local' })
+require("dotenv").config({ path: __dirname + "/.env.local" });
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
@@ -10,33 +10,34 @@ module.exports = {
       {
         version: "0.8.17",
       },
-    ]
-  }, defaultNetwork: "mumbai",
+    ],
+  },
+  defaultNetwork: "mumbai",
   networks: {
     hardhat: {},
     mumbai: {
-      url: "https://matic-mumbai.chainstacklabs.com	",
-      accounts: [process.env.NEXT_PUBLIC_PRIVATEKEY]
+      url: "https://matic-mumbai.chainstacklabs.com",
+      accounts: [process.env.NEXT_PUBLIC_PRIVATEKEY],
     },
     filecoinHyperspace: {
       url: "https://api.hyperspace.node.glif.io/rpc/v1",
-      accounts: [process.env.NEXT_PUBLIC_PRIVATEKEY]
+      accounts: [process.env.NEXT_PUBLIC_PRIVATEKEY],
     },
     gnosis: {
       url: "https://rpc.gnosischain.com",
-      accounts: [process.env.NEXT_PUBLIC_PRIVATEKEY]
+      accounts: [process.env.NEXT_PUBLIC_PRIVATEKEY],
     },
     scroll: {
       url: "https://alpha-rpc.scroll.io/l2",
-      accounts: [process.env.NEXT_PUBLIC_PRIVATEKEY]
+      accounts: [process.env.NEXT_PUBLIC_PRIVATEKEY],
     },
     polygonZK: {
       url: "https://rpc.public.zkevm-test.net",
-      accounts: [process.env.NEXT_PUBLIC_PRIVATEKEY]
+      accounts: [process.env.NEXT_PUBLIC_PRIVATEKEY],
     },
     optimism: {
       url: "https://endpoints.omniatech.io/v1/op/goerli/public",
-      accounts: [process.env.NEXT_PUBLIC_PRIVATEKEY]
-    }
+      accounts: [process.env.NEXT_PUBLIC_PRIVATEKEY],
+    },
   },
 };
