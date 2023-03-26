c8130f1b46074d9bf957416c0f831c145f9f2f59
parth <parthsheth469@gmail.com>
2023-03-26 19:45:00 +0530

    getEnumMapping

--- src/app/api/networks/getEnumMapping/route.js
+++ src/app/api/networks/getEnumMapping/route.js
@@ -4,9 +4,8 @@ import { NextResponse } from "next/server";
 // network information
 const NetworkArray = ["filecoinHyperspace", "gnosis", "scroll", "optimism"];
 
-
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const network = searchParams.get("index");
-  return NextResponse.json(NetworkArray[`${network}`]);
+  return NextResponse.json({ network: NetworkArray[`${network}`] });
 }
