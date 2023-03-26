52ca76983c857b22d892cc93e076287c5b252110
Ullas Rajesh <ullasrajesh23@gmail.com>
2023-03-26 07:21:18 +0530

    Modified login card

--- src/app/ngo/page.js
+++ src/app/ngo/page.js
@@ -3,6 +3,7 @@
 import React from "react";
 import Image from "next/image";
 import LandingCard from "@/components/UI/LandingCard";
+import GoBackButton from "@/components/UI/GoBackButton";
 export default function page() {
   return (
     <>
