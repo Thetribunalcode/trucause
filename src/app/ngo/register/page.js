01ebc03729217f626530863e70fb0df629a2dfca
parth <parthsheth469@gmail.com>
2023-03-26 14:15:24 +0530

    added pinata

--- src/app/ngo/register/page.js
+++ src/app/ngo/register/page.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import React from "react";
 import Register from "../../../components/Pages/Register";
@@ -9,22 +9,17 @@ import UploadTest from "../../../components/Pages/UploadTest";
 export default function page() {
   return (
     <>
-      <div className='flex flex-row justify-center items-center mt-44 mb-44'>
+      {/* <div className='flex flex-row justify-center items-center mt-44 mb-44'>
         <UploadTest />
-      </div>
-      <div className='flex flex-row justify-center items-center mt-44 mb-44'>
-        <div className='mr-44'>
-          <div className='mb-10'>
+      </div> */}
+      <div className="flex flex-row justify-center items-center mt-44 mb-44">
+        <div className="mr-44">
+          <div className="mb-10">
             <GoBackButton />
           </div>
           <Register />
         </div>
-        <Image
-          src='/register.png'
-          height={500}
-          width={500}
-          alt=''
-        />
+        <Image src="/register.png" height={500} width={500} alt="" />
       </div>
     </>
   );
